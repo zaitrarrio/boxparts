f0a6698f2a84aabcd2574f936aec10e342403a4c
epicDistortion <epicdistortion@gmail.com>
2014-06-26 02:24:33 +0000

    Added rust-nightly package that installs from rust-lang.org installer, version 0.11.0-pre-nightly (05ca9f7 2014-06-24)

---
+++ lib/autoparts/packages/rust_nightly.rb
@@ -0,0 +1,24 @@
+# Copyright (c) 2013-2014 Irrational Industries Inc. d.b.a. Nitrous.IO
+# This software is licensed under the [BSD 2-Clause license](https://raw.github.com/nitrous-io/autoparts/master/LICENSE).
+
+module Autoparts
+  module Packages
+    class Rust < Package
+      name 'rust-nightly'
+      version '0.11.0-pre-nightly-05ca9f7_2014-06-24'
+      description 'Rust: A safe, concurrent, practical language'
+      category Category::PROGRAMMING_LANGUAGES
+
+      source_url 'http://static.rust-lang.org/dist/rust-nightly-x86_64-unknown-linux-gnu.tar.gz'
+      source_sha1 'e20ad572b5266b915ffe0142322c256927f15a4e'
+      source_filetype 'tar.gz'
+
+      def install
+        Dir.chdir('rust-nightly-x86_64-unknown-linux-gnu') do
+          execute './install.sh', "--prefix=#{prefix_path}"
+        end
+      end
+
+    end
+  end
+end
