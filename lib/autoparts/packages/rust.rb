eab928c3abbe06567aba0e666261c6b32993807a
epicDistortion <epicdistortion@gmail.com>
2014-07-04 22:20:32 +0000

    updated rust binary installer to 0.11.0

--- lib/autoparts/packages/rust.rb
+++ lib/autoparts/packages/rust.rb
@@ -5,16 +5,16 @@ module Autoparts
   module Packages
     class Rust < Package
       name 'rust'
-      version '0.10'
+      version '0.11.0'
       description 'Rust: A safe, concurrent, practical language'
       category Category::PROGRAMMING_LANGUAGES
 
-      source_url 'http://static.rust-lang.org/dist/rust-0.10-x86_64-unknown-linux-gnu.tar.gz'
-      source_sha1 '6c2a7c233912e4a02784f284aa2b24e37c6243d4'
+      source_url 'http://static.rust-lang.org/dist/rust-0.11.0-x86_64-unknown-linux-gnu.tar.gz'
+      source_sha1 '5e3c89783ba148cbc0f4ba9bd6cc2c5a5e056830'
       source_filetype 'tar.gz'
 
       def install
-        Dir.chdir('rust-0.10-x86_64-unknown-linux-gnu') do
+        Dir.chdir('rust-0.11.0-x86_64-unknown-linux-gnu') do
           execute './install.sh', "--prefix=#{prefix_path}"
         end
       end
