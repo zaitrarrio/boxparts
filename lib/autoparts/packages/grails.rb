cdb9235136f026a80ddea9b48ebc3ca2fcb3afa3
maximka <maxim.kraev@gmail.com>
2014-08-07 04:58:12 +0000

    grails updated, resolves #176

--- lib/autoparts/packages/grails.rb
+++ lib/autoparts/packages/grails.rb
@@ -2,10 +2,10 @@ module Autoparts
   module Packages
     class Grails < Package
       name 'grails'
-      version '2.3.7'
+      version '2.4.3'
       description 'Grails: an Open Source, full stack, web application framework for the JVM.'
-      source_url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.3.7.zip'
-      source_sha1 '295f37fe989e8b3784258fcc14d2749737d8a2d0'
+      source_url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.4.3.zip'
+      source_sha1 '729cc9f2493960c3a644f29208d3b8a8f31275be'
       source_filetype 'zip'
       category Category::DEVELOPMENT_TOOLS
 
@@ -19,21 +19,20 @@ module Autoparts
         Dir.glob(prefix_path.to_s + '/bin/*.bat').each { |f| File.delete(f) }
       end
 
-      def env_content
-        <<-EOS.unindent
-          export JAVA_HOME="/usr"
-          export GRAILS_HOME="#{prefix_path}"
-        EOS
-      end
+	  def required_env
+        env_file.unlink if env_file.exist?
+        [
+          'export JAVA_HOME="/usr"',
+          "export GRAILS_HOME='#{prefix_path}'"
+        ]
+      end        
+        
+        
       def env_file
         Path.env + 'grails'
       end
 
       def post_install
-        File.write(env_file, env_content)
-      end
-
-      def post_uninstall
         env_file.unlink if env_file.exist?
       end
 
