f038fcb472a1a55d32c200922f6117fe0e333f0b
maximka <maxim.kraev@gmail.com>
2014-09-09 05:57:47 +0000

    postgres version update + uuid support, resolves #166

--- lib/autoparts/packages/postgresql.rb
+++ lib/autoparts/packages/postgresql.rb
@@ -5,16 +5,18 @@ module Autoparts
   module Packages
     class PostgreSQL < Package
       name 'postgresql'
-      version '9.3.4'
+      version '9.3.5'
       description "PostgreSQL: The world's most advanced open-source database system"
       category Category::DATA_STORES
 
-      source_url 'http://ftp.postgresql.org/pub/source/v9.3.4/postgresql-9.3.4.tar.gz'
-      source_sha1 '9f80b3cfebc434ca0860fc8489923cea73456081'
+      source_url 'http://ftp.postgresql.org/pub/source/9.3.5/postgresql-9.3.5.tar.gz'
+      source_sha1 'f5a888aaba98d637fa6cdf009aebcda10d53d038'
       source_filetype 'tar.gz'
 
+      depends_on 'uuid'
+      
       def compile
-        Dir.chdir('postgresql-9.3.4') do
+        Dir.chdir(name_with_version) do
           args = [
             '--disable-debug',
             "--prefix=#{prefix_path}",
@@ -31,7 +33,8 @@ module Autoparts
             '--with-krb5',
             '--with-libxml',
             '--with-libxslt',
-            '--with-openssl'
+            '--with-openssl',
+            '--with-ossp-uuid',
           ]
 
           execute './configure', *args
@@ -40,7 +43,7 @@ module Autoparts
       end
 
       def install
-        Dir.chdir('postgresql-9.3.4') do
+        Dir.chdir(name_with_version) do
           execute 'make install-world'
         end
       end
